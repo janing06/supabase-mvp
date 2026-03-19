d970189de10b97c6451c92255177537c221ab93b
janing06 <jhonmichaelturija@gmail.com>
2026-03-19 12:55:46 +0800

    fix: no verify jwt

--- supabase/functions/upload-avatar/index.ts
+++ supabase/functions/upload-avatar/index.ts
@@ -6,10 +6,6 @@ const corsHeaders = {
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 }
  
-export const config = {
-  verify_jwt: false,
-};
-
 Deno.serve(async (req) => {
   if (req.method === "OPTIONS") {
     return new Response(null, { headers: corsHeaders })
